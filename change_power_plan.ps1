f0ee794027144d8883ac1cddd1e960b38920d98b
mino-canon <mino_canon_0899@yahoo.co.jp>
2024-02-25 00:43:45 +0900

    引数にプランを指定できるようにした

--- change_power_plan.ps1
+++ change_power_plan.ps1
@@ -1,29 +1,28 @@
-#!/bin/bash
+param([String]$input_plan)
+if (-not $input_plan) {
+  $input_plan = Read-Host "Please input power plan [high or low or balance]. Or please input [show] to see power plans."
+}
 
 # read setting
 . ./conf.ps1
 
-# input
-$input = read-host "please input power plan [high or low or balance].or please input [show] to see power plans."
-
-# inut logic
+# input logic
 if($input -eq "show"){
   powercfg /list
   pause
   exit
-}elseif(!$INPUT_RELATION.ContainsKey($input)){
+}elseif(!$INPUT_RELATION.ContainsKey($input_plan)){
   echo "inputed in not exact power plan. please check conf.ps INPUT_RELATION."
   pause
   exit
 }
-$select_plan = $INPUT_RELATION[$input]
+$select_plan = $INPUT_RELATION[$input_plan]
 
 # execute
 powercfg /s $select_plan
 
 # show result
 if($?){
-  $result = "success!!"
   exit
 }
 
